bd85361540b56f0aadc18e03dad75cb3c863968c
Zedan-gxt6 <zedanblr@gmail.com>
2026-06-17 22:26:30 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> curr(2,vector<int>(2,0));
+        vector<vector<int>> next(2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            curr[0][0]=max(-prices[i]+next[1][0],next[0][0]);
+            curr[0][1]=next[0][0];
+            curr[1][0]=curr[1][1]=max(prices[i]+next[0][1],next[1][1]);
+            next=curr;
+        }
+        return curr[0][0];
+    }
+};
